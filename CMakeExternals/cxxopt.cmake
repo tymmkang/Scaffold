3861e3d1c5e2c80714cbb8613f45f279e58dc5d2
tymmkang <tymmkang@gmail.com>
2021-12-28 00:37:35 +0900

    cxxopt 외부 모듈 cmake 파일 개선

--- CMakeExternals/cxxopt.cmake
+++ CMakeExternals/cxxopt.cmake
@@ -17,14 +17,32 @@ function(SCAF_EM_FUNC_ADD_CXXOPT
         GIT_TAG ${VAR_EXTERNAL_GIT_TAG}
     )
 
-    FetchContent_GetProperties(${VAR_EXTERNAL_NAME})
-
-    if(NOT ${VAR_EXTERNAL_NAME}_POPULATED)
+    # FetchContent_MakeAvailable(${VAR_EXTERNAL_NAME})
+    FetchContent_GetProperties(${VAR_EXTERNAL_NAME} POPULATED VAR_POPULATED)
+    
+    if(NOT ${VAR_POPULATED})
         message(STATUS "Fetch '${VAR_EXTERNAL_NAME}' from '${VAR_EXTERNAL_GIT_REPO}' (${VAR_EXTERNAL_GIT_TAG})")
+        
         FetchContent_Populate(${VAR_EXTERNAL_NAME})
+        FetchContent_GetProperties(${VAR_EXTERNAL_NAME}
+            SOURCE_DIR VAR_SOURCE_DIR
+            BINARY_DIR VAR_BINARY_DIR
+        )
+
+        if(EXISTS "${SCAF_EM_OUTPUT_DIR}/${VAR_EXTERNAL_NAME}")
+            file(REMOVE "${SCAF_EM_OUTPUT_DIR}/${VAR_EXTERNAL_NAME}")
+        endif()
+
+        file(GLOB VAR_SOURCES 
+            "${VAR_SOURCE_DIR}/include/*h"
+            "${VAR_SOURCE_DIR}/include/*hpp")
+
+        file(COPY "${VAR_SOURCES}" 
+            DESTINATION "${SCAF_EM_OUTPUT_DIR}/${VAR_EXTERNAL_NAME}/include/${VAR_EXTERNAL_NAME}")
+
         message(STATUS "Fetch '${VAR_EXTERNAL_NAME}' Done")
     endif()
 
-    
-
+    target_include_directories(${TARGET_MODULE} PRIVATE "${SCAF_EM_OUTPUT_DIR}/${VAR_EXTERNAL_NAME}/include")
+    message(STATUS "Add '${VAR_EXTERNAL_NAME}' to '${TARGET_MODULE}'")
 endfunction()
