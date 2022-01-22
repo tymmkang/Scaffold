deae3c82be2fa878419d1243a8c7218bdab97a63
tymmkang <tymmkang@gmail.com>
2022-01-23 07:48:47 +0900

    Vulkan 외부 모듈 추가 cmake 스크립트 작성

---
+++ CMakeExternals/vulkan.cmake
@@ -0,0 +1,23 @@
+# Vulkan
+# Official website : https://www.vulkan.org/
+
+function(SCAF_EM_FUNC_ADD_VULKAN 
+    TARGET_MODULE)
+
+    if (NOT CMAKE_VERSION VERSION_LESS 3.7.0)
+        find_package(Vulkan)
+
+        if(${Vulkan_FOUND})
+            target_link_libraries(${TARGET_MODULE} PUBLIC ${Vulkan_LIBRARIES})
+            include_directories(${Vulkan_INCLUDE_DIRS})
+        else()
+            message(FATAL_ERROR "Not found Vulkan SDK")
+        endif()
+
+    elseif()
+        message(FATAL_ERROR "Can not use Vulkan CMake package. Upgrade CMake version to above 3.7.0")
+    endif()
+
+    message(STATUS "Add 'Vulkan' external dependency to '${TARGET_MODULE}'")    
+
+endfunction()
