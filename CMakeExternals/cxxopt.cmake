ae080437b7694f4128c9e9fa7cf37ed8d15def57
tymmkang <tymmkang@gmail.com>
2022-02-04 10:03:10 +0900

    Header-Only 외부 모듈 추가 스크립트 리팩터링

--- CMakeExternals/cxxopt.cmake
+++ CMakeExternals/cxxopt.cmake
@@ -3,46 +3,36 @@
 # repo : https://github.com/jarro2783/cxxopts
 # Header-Only
 
-# 외부 CMake 프로젝트를 특정 프로젝트에 추가합니다.
-# TARGET_MODULE [IN]   외부 프로젝트에 의존 할 모듈
 function(SCAF_EM_FUNC_ADD_CXXOPT 
     TARGET_MODULE)
 
-    set(VAR_EXTERNAL_NAME       "cxxopt")
-    set(VAR_EXTERNAL_GIT_REPO   "https://github.com/jarro2783/cxxopts.git")
-    set(VAR_EXTERNAL_GIT_TAG    "v2.2.1")
+    set(VAR_EXTERNAL_NAME           "cxxopt")
+    set(VAR_EXTERNAL_GIT_REPO_URL   "https://github.com/jarro2783/cxxopts.git")
+    set(VAR_EXTERNAL_GIT_TAG        "v2.2.1")
 
     FetchContent_Declare(${VAR_EXTERNAL_NAME}
-        GIT_REPOSITORY ${VAR_EXTERNAL_GIT_REPO}
+        GIT_REPOSITORY ${VAR_EXTERNAL_GIT_REPO_URL}
         GIT_TAG ${VAR_EXTERNAL_GIT_TAG}
     )
 
-    # FetchContent_MakeAvailable(${VAR_EXTERNAL_NAME})
-    FetchContent_GetProperties(${VAR_EXTERNAL_NAME} POPULATED VAR_POPULATED)
-    
-    if(NOT ${VAR_POPULATED})
-        message(STATUS "Fetch '${VAR_EXTERNAL_NAME}' from '${VAR_EXTERNAL_GIT_REPO}' (${VAR_EXTERNAL_GIT_TAG})")
-        
+    FetchContent_GetProperties(${VAR_EXTERNAL_NAME})
+    if (NOT "${${VAR_EXTERNAL_NAME}_POPULATED}")
+        message(STATUS "Clone '${VAR_EXTERNAL_NAME}' from '${VAR_EXTERNAL_GIT_REPO_URL}'")
         FetchContent_Populate(${VAR_EXTERNAL_NAME})
-        FetchContent_GetProperties(${VAR_EXTERNAL_NAME}
-            SOURCE_DIR VAR_SOURCE_DIR
-            BINARY_DIR VAR_BINARY_DIR
-        )
+        message(STATUS "Clone '${VAR_EXTERNAL_NAME}' Done")
+        
+        if (EXISTS "${FETCHCONTENT_BASE_DIR}/${VAR_EXTERNAL_NAME}")
+            file(REMOVE "${FETCHCONTENT_BASE_DIR}/${VAR_EXTERNAL_NAME}")
+        endif ()
 
-        if(EXISTS "${SCAF_EM_OUTPUT_DIR}/${VAR_EXTERNAL_NAME}")
-            file(REMOVE "${SCAF_EM_OUTPUT_DIR}/${VAR_EXTERNAL_NAME}")
-        endif()
+        # tymmkang@gmail.com 2022-02-04
+        # NOTE : 리포지토리 별로 디렉토리 구조가 다르기 때문에 신경 써 줘야 합니다.
+        file(GLOB VAR_FILES 
+            "${${VAR_EXTERNAL_NAME}_SOURCE_DIR}/include/*")
+        file(COPY "${VAR_FILES}" 
+            DESTINATION "${SCAF_VAR_EXTERNAL_MODULE_PREFIX_DIR}/${VAR_EXTERNAL_NAME}/include/${VAR_EXTERNAL_NAME}")
+    endif ()
 
-        file(GLOB VAR_SOURCES 
-            "${VAR_SOURCE_DIR}/include/*h"
-            "${VAR_SOURCE_DIR}/include/*hpp")
-
-        file(COPY "${VAR_SOURCES}" 
-            DESTINATION "${SCAF_EM_OUTPUT_DIR}/${VAR_EXTERNAL_NAME}/include/${VAR_EXTERNAL_NAME}")
-
-        message(STATUS "Fetch '${VAR_EXTERNAL_NAME}' Done")
-    endif()
-
-    target_include_directories(${TARGET_MODULE} PRIVATE "${SCAF_EM_OUTPUT_DIR}/${VAR_EXTERNAL_NAME}/include")
-    message(STATUS "Add '${VAR_EXTERNAL_NAME}' to '${TARGET_MODULE}'")
+    message(STATUS "Add '${VAR_EXTERNAL_NAME}' external dependency to '${TARGET_MODULE}'") 
+    target_include_directories(${TARGET_MODULE} PRIVATE "${SCAF_VAR_EXTERNAL_MODULE_PREFIX_DIR}/${VAR_EXTERNAL_NAME}/include")
 endfunction()
--- CMakeExternals/glm.cmake
+++ CMakeExternals/glm.cmake
@@ -17,14 +17,17 @@ function(SCAF_EM_FUNC_ADD_GLM
     FetchContent_GetProperties(${VAR_EXTERNAL_NAME})
     if (NOT "${${VAR_EXTERNAL_NAME}_POPULATED}")
         message(STATUS "Clone '${VAR_EXTERNAL_NAME}' from '${VAR_EXTERNAL_GIT_REPO_URL}'")
-        FetchContent_MakeAvailable(${VAR_EXTERNAL_NAME})
+        FetchContent_Populate(${VAR_EXTERNAL_NAME})
         message(STATUS "Clone '${VAR_EXTERNAL_NAME}' Done")
 
         if (EXISTS "${FETCHCONTENT_BASE_DIR}/${VAR_EXTERNAL_NAME}")
             file(REMOVE "${FETCHCONTENT_BASE_DIR}/${VAR_EXTERNAL_NAME}")
         endif ()
 
-        file(COPY "${${VAR_EXTERNAL_NAME}_SOURCE_DIR}/glm" DESTINATION "${SCAF_VAR_EXTERNAL_MODULE_PREFIX_DIR}/${VAR_EXTERNAL_NAME}/include")
+        # tymmkang@gmail.com 2022-02-04
+        # NOTE : 리포지토리 별로 디렉토리 구조가 다르기 때문에 신경 써 줘야 합니다.
+        file(COPY "${${VAR_EXTERNAL_NAME}_SOURCE_DIR}/${VAR_EXTERNAL_NAME}" 
+            DESTINATION "${SCAF_VAR_EXTERNAL_MODULE_PREFIX_DIR}/${VAR_EXTERNAL_NAME}/include")
     endif ()
 
     message(STATUS "Add '${VAR_EXTERNAL_NAME}' external dependency to '${TARGET_MODULE}'") 
