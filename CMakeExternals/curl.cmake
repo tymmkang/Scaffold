a295bb7c11307a598e47e4d11000e5fe8374b5be
tymmkang <tymmkang@gmail.com>
2022-02-04 21:33:10 +0900

    외부 모듈을 중복참조 할 수 있도록 수정

--- CMakeExternals/curl.cmake
+++ CMakeExternals/curl.cmake
@@ -13,31 +13,40 @@ function(SCAF_EM_FUNC_ADD_CURL
     set(VAR_EXTERNAL_GIT_TAG        "curl-7_81_0")
     set(VAR_EXTERNAL_SHARED         OFF)
 
-    ExternalProject_Add(${VAR_EXTERNAL_NAME} 
-        GIT_REPOSITORY ${VAR_EXTERNAL_GIT_REPO_URL}
-        GIT_TAG ${VAR_EXTERNAL_GIT_TAG}
+    if (NOT TARGET ${VAR_EXTERNAL_NAME})
 
-        PREFIX ${FETCHCONTENT_BASE_DIR}/${VAR_EXTERNAL_NAME}
-        INSTALL_DIR ${SCAF_VAR_EXTERNAL_MODULE_PREFIX_DIR}/${VAR_EXTERNAL_NAME}
+        ExternalProject_Add(${VAR_EXTERNAL_NAME} 
+            GIT_REPOSITORY ${VAR_EXTERNAL_GIT_REPO_URL}
+            GIT_TAG ${VAR_EXTERNAL_GIT_TAG}
 
-        BUILD_COMMAND ""
+            PREFIX ${FETCHCONTENT_BASE_DIR}/${VAR_EXTERNAL_NAME}
+            INSTALL_DIR ${SCAF_VAR_EXTERNAL_MODULE_PREFIX_DIR}/${VAR_EXTERNAL_NAME}
 
-        CMAKE_ARGS
-        "-DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>"
-        "-DCMAKE_BUILD_TYPE=$<$<CONFIG:Release>:Release>$<$<CONFIG:Debug>:Debug>"
-        "-DCMAKE_CONFIGURATION_TYPES=$<$<CONFIG:Release>:Release>$<$<CONFIG:Debug>:Debug>"
-        "-DBUILD_SHARED_LIBS=${VAR_EXTERNAL_SHARED}"
-        "-DBUILD_CURL_EXE=OFF"
-        # 이 외에도 옵션이 많은데, 잘 모르겠어서 건드리지 않고 있습니다.
+            BUILD_COMMAND ""
 
-        INSTALL_COMMAND
-            ${CMAKE_COMMAND}
-            --build .
-            --target install
-            --config $<$<CONFIG:Release>:Release>$<$<CONFIG:Debug>:Debug>
+            CMAKE_ARGS
+            "-DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>"
+            "-DCMAKE_BUILD_TYPE=$<$<CONFIG:Release>:Release>$<$<CONFIG:Debug>:Debug>"
+            "-DCMAKE_CONFIGURATION_TYPES=$<$<CONFIG:Release>:Release>$<$<CONFIG:Debug>:Debug>"
+            "-DBUILD_SHARED_LIBS=${VAR_EXTERNAL_SHARED}"
+            "-DBUILD_CURL_EXE=OFF"
+            # 이 외에도 옵션이 많은데, 잘 모르겠어서 건드리지 않고 있습니다.
 
-        LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
-    )
+            INSTALL_COMMAND
+                ${CMAKE_COMMAND}
+                --build .
+                --target install
+                --config $<$<CONFIG:Release>:Release>$<$<CONFIG:Debug>:Debug>
+
+            LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
+        )
+
+        set_target_properties(${VAR_EXTERNAL_NAME} PROPERTIES FOLDER ${SCAF_VAR_EXTERNAL_MODULE_RELATIVE_DIR})
+
+        if (${VAR_EXTERNAL_SHARED})
+            # TODO : Copy shared library to project output
+        endif ()
+    endif ()
 
     message(STATUS "Add '${VAR_EXTERNAL_NAME}' external dependency to '${TARGET_MODULE}'") 
 
@@ -46,11 +55,8 @@ function(SCAF_EM_FUNC_ADD_CURL
     target_include_directories(${TARGET_MODULE} PRIVATE "${INSTALL_DIR}/include")
     target_link_libraries(${TARGET_MODULE} PRIVATE "${INSTALL_DIR}/lib/$<$<CONFIG:Release>:libcurl>$<$<CONFIG:Debug>:libcurl-d>${CMAKE_STATIC_LIBRARY_SUFFIX}")
 
-    if (${VAR_EXTERNAL_SHARED})
-        # TODO : Copy shared library to project output
-    else ()
+    if (NOT ${VAR_EXTERNAL_SHARED})
         target_compile_definitions(${TARGET_MODULE} PRIVATE "CURL_STATICLIB")
     endif ()
 
-    set_target_properties(${VAR_EXTERNAL_NAME} PROPERTIES FOLDER ${SCAF_VAR_EXTERNAL_MODULE_RELATIVE_DIR})
 endfunction()
--- CMakeExternals/glfw.cmake
+++ CMakeExternals/glfw.cmake
@@ -13,37 +13,46 @@ function(SCAF_EM_FUNC_ADD_GLFW
     set(VAR_EXTERNAL_GIT_TAG        "3.3.6")
     set(VAR_EXTERNAL_SHARED         OFF)
 
-    ExternalProject_Add(${VAR_EXTERNAL_NAME} 
-        GIT_REPOSITORY ${VAR_EXTERNAL_GIT_REPO_URL}
-        GIT_TAG ${VAR_EXTERNAL_GIT_TAG}
+    if (NOT TARGET ${VAR_EXTERNAL_NAME})
 
-        PREFIX ${FETCHCONTENT_BASE_DIR}/${VAR_EXTERNAL_NAME}
-        INSTALL_DIR ${SCAF_VAR_EXTERNAL_MODULE_PREFIX_DIR}/${VAR_EXTERNAL_NAME}
-    
-        BUILD_COMMAND ""
-    
-        CMAKE_ARGS
-            "-DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>"
-            "-DCMAKE_BUILD_TYPE=$<$<CONFIG:Release>:Release>$<$<CONFIG:Debug>:Debug>"
-            "-DCMAKE_CONFIGURATION_TYPES=$<$<CONFIG:Release>:Release>$<$<CONFIG:Debug>:Debug>"
-            "-DBUILD_SHARED_LIBS=${VAR_EXTERNAL_SHARED}"
-            "-DGLFW_BUILD_EXAMPLES=OFF"
-            "-DGLFW_BUILD_TESTS=OFF"
-            "-DGLFW_BUILD_DOCS=OFF"
-    
-        INSTALL_COMMAND
-            ${CMAKE_COMMAND}
-            --build .
-            --target install
-            --config $<$<CONFIG:Release>:Release>$<$<CONFIG:Debug>:Debug>
+        ExternalProject_Add(${VAR_EXTERNAL_NAME} 
+            GIT_REPOSITORY ${VAR_EXTERNAL_GIT_REPO_URL}
+            GIT_TAG ${VAR_EXTERNAL_GIT_TAG}
 
-        # tymmkang@gmail.com 2022-02-04
-        # NOTE : 개인 리포지토리에서 마이그레이션 할 때 이런 인자를 사용했었습니다.
-        # 어렴풋이 기억나는 이유는 VSCode 개발환경에서 Ninja 제너레이터를 사용하는 것과 관련이 있었던 것 같기도...   
-        # BUILD_BYPRODUCTS "${INSTALL_DIR}/lib/glfw3.lib"
+            PREFIX ${FETCHCONTENT_BASE_DIR}/${VAR_EXTERNAL_NAME}
+            INSTALL_DIR ${SCAF_VAR_EXTERNAL_MODULE_PREFIX_DIR}/${VAR_EXTERNAL_NAME}
         
-        LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
-    )
+            BUILD_COMMAND ""
+        
+            CMAKE_ARGS
+                "-DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>"
+                "-DCMAKE_BUILD_TYPE=$<$<CONFIG:Release>:Release>$<$<CONFIG:Debug>:Debug>"
+                "-DCMAKE_CONFIGURATION_TYPES=$<$<CONFIG:Release>:Release>$<$<CONFIG:Debug>:Debug>"
+                "-DBUILD_SHARED_LIBS=${VAR_EXTERNAL_SHARED}"
+                "-DGLFW_BUILD_EXAMPLES=OFF"
+                "-DGLFW_BUILD_TESTS=OFF"
+                "-DGLFW_BUILD_DOCS=OFF"
+        
+            INSTALL_COMMAND
+                ${CMAKE_COMMAND}
+                --build .
+                --target install
+                --config $<$<CONFIG:Release>:Release>$<$<CONFIG:Debug>:Debug>
+
+            # tymmkang@gmail.com 2022-02-04
+            # NOTE : 개인 리포지토리에서 마이그레이션 할 때 이런 인자를 사용했었습니다.
+            # 어렴풋이 기억나는 이유는 VSCode 개발환경에서 Ninja 제너레이터를 사용하는 것과 관련이 있었던 것 같기도...   
+            # BUILD_BYPRODUCTS "${INSTALL_DIR}/lib/glfw3.lib"
+            
+            LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
+        )
+
+        set_target_properties(${VAR_EXTERNAL_NAME} PROPERTIES FOLDER ${SCAF_VAR_EXTERNAL_MODULE_RELATIVE_DIR})
+
+        # if (${VAR_EXTERNAL_SHARED})
+        #     # TODO : Copy shared library to project output
+        # endif ()
+    endif ()
 
     message(STATUS "Add '${VAR_EXTERNAL_NAME}' external dependency to '${TARGET_MODULE}'") 
 
@@ -52,9 +61,4 @@ function(SCAF_EM_FUNC_ADD_GLFW
     target_include_directories(${TARGET_MODULE} PRIVATE "${INSTALL_DIR}/include")
     target_link_libraries(${TARGET_MODULE} PRIVATE "${INSTALL_DIR}/lib/glfw3${CMAKE_STATIC_LIBRARY_SUFFIX}")
 
-    # if (${VAR_EXTERNAL_SHARED})
-    #     # TODO : Copy shared library to project output
-    # endif ()
-
-    set_target_properties(${VAR_EXTERNAL_NAME} PROPERTIES FOLDER ${SCAF_VAR_EXTERNAL_MODULE_RELATIVE_DIR})
 endfunction()
