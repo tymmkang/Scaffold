7839dccfd095f6e8531fbaf9a0c3a30254cfc9bf
tymmkang <tymmkang@gmail.com>
2022-02-04 10:03:23 +0900

    Vulkan 외부 모듈 추가 스크립트 리팩터링

--- CMakeExternals/vulkan.cmake
+++ CMakeExternals/vulkan.cmake
@@ -1,23 +1,26 @@
 # Vulkan
 # Official website : https://www.vulkan.org/
+# CMake-Package
 
 function(SCAF_EM_FUNC_ADD_VULKAN 
     TARGET_MODULE)
 
+    set(VAR_EXTERNAL_NAME "Vulkan")
+
     if (NOT CMAKE_VERSION VERSION_LESS 3.7.0)
-        find_package(Vulkan QUIET)
+        find_package(${VAR_EXTERNAL_NAME} QUIET)
 
-        if(${Vulkan_FOUND})
-            target_link_libraries(${TARGET_MODULE} PUBLIC ${Vulkan_LIBRARIES})
-            include_directories(${Vulkan_INCLUDE_DIRS})
-        else()
-            message(FATAL_ERROR "Not found Vulkan SDK")
-        endif()
+        if ("${${VAR_EXTERNAL_NAME}_FOUND}")
+            target_link_libraries(${TARGET_MODULE} PRIVATE "${${VAR_EXTERNAL_NAME}_LIBRARIES}")
+            include_directories("${${VAR_EXTERNAL_NAME}_INCLUDE_DIRS}")
+        else ()
+            message(FATAL_ERROR "Not found ${VAR_EXTERNAL_NAME}")
+        endif ()
 
-    elseif()
-        message(FATAL_ERROR "Can not use Vulkan CMake package. Upgrade CMake version to above 3.7.0")
-    endif()
+    elseif ()
+        message(FATAL_ERROR "Can not use ${VAR_EXTERNAL_NAME} CMake package. Upgrade CMake version to above 3.7.0")
+    endif ()
 
-    message(STATUS "Add 'Vulkan' external dependency to '${TARGET_MODULE}'")    
+    message (STATUS "Add '${VAR_EXTERNAL_NAME}' external dependency to '${TARGET_MODULE}'")    
 
 endfunction()
